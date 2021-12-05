9078478516c522811cebdb0e45b38b1781df834e
Hung Tran <hungtran0203@gmail.com>
2021-12-05 08:25:59 +0700

    update env

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,6 @@ const gbConfig = gatsbyMergeConfig(
         resolve: 'gatsby-source-contentful',
         options: {
           spaceId: process.env.CONTENTFUL_SPACE_ID_WEBSITE,
-          // Learn about environment variables: https://gatsby.dev/env-vars
           accessToken: process.env.CONTENTFUL_ACCESS_TOKEN_WEBSITE,
         },
       },
