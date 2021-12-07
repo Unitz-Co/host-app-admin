59d9811a01ee6cbe5889316ebb1d06e341cd4b20
Phuong Vo <vlaphuong1991@gmail.com>
2021-12-07 21:06:15 +0700

    add user tester page

---
+++ src/templates/user/tester/.meta.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const _ = require('lodash');
+require('@vl/mod-config/web');
+
+exports.createPages = async (item, gatsby) => {
+  // @update query
+  return Promise.all(
+    ['index'].map(() => {
+      const pagePath = 'user/tester';
+      console.log('creating page', pagePath);
+      const pageContext = _.cloneDeep({
+        id: pagePath,
+        slug: pagePath,
+        params: {
+          navbar: {
+            title: 'Users/Testers',
+            path: 'user/tester',
+          },
+        },
+      });
+      return gatsby.actions.createPage({
+        path: pagePath,
+        component: item.resolvers.component(gatsby),
+        context: pageContext,
+      });
+    })
+  );
+};
---
+++ src/templates/user/tester/.route.js
@@ -0,0 +1,19 @@
+const _ = require('lodash');
+const slugify = require('slugify');
+const { routeStore } = require('@vl/mod-utils/gatsbyRouteStore');
+
+routeStore.addRule('ticket', {
+  url: (params) => {
+    return `/support/ticket`;
+  },
+  parse: (urlObject) => {
+    const params = {};
+    for (let param in urlObject.searchParams) {
+      params[param] = urlObject.searchParams.get(param);
+    }
+    return params;
+  },
+  match: (urlObject) => {
+    return urlObject.pathname === 'ticket';
+  },
+});
