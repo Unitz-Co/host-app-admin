7f750fcb10060d9850440697310062469c172ef1
Phuong Vo <vlaphuong1991@gmail.com>
2021-12-27 16:03:18 +0700

    update

--- src/templates/advisor/profile/.route.js
+++ src/templates/advisor/profile/.route.js
@@ -39,3 +39,34 @@ routeStore.addRule('profile:user', {
     return `${routeStore.getAppOrigin('user')}${_.get(params, 'profile.slug')}`;
   },
 });
+
+routeStore.addRule('advisor', {
+  url: (params) => {
+    // from slug
+    const slug = _.get(params, 'slug') || _.get(params, 'profile.slug');
+    if (slug) {
+      return slug;
+    }
+    // from advisor displayName
+    const displayName = _.get(params, 'displayName') || _.get(params, 'display_name');
+    if (displayName) {
+      return `/advisor?${slugify(displayName)}`;
+    }
+    // from advisorId
+    const advisorId = _.get(params, 'id') || _.get(params, 'advisor_id');
+    if (advisorId) {
+      return `/advisor/${advisorId}`;
+    }
+    return `/advisor`;
+  },
+  parse: (urlObject) => {
+    const params = {};
+    for (let param in urlObject.searchParams) {
+      params[param] = urlObject.searchParams.get(param);
+    }
+    return params;
+  },
+  match: (urlObject) => {
+    return urlObject.pathname === 'advisor';
+  },
+});
--- src/templates/user/profile/.route.js
+++ src/templates/user/profile/.route.js
@@ -1,7 +1,7 @@
 const _ = require('lodash');
 const slugify = require('slugify');
 const { routeStore } = require('@vl/mod-utils/gatsbyRouteStore');
-
+const querystring = require('querystring');
 routeStore.addRule('user/profile', {
   url: (params) => {
     return `/user/profile?id=${_.get(params, 'id')}`;
@@ -37,5 +37,46 @@ routeStore.addRule('profile', {
 routeStore.addRule('profile:user', {
   url: (params) => {
     return `${routeStore.getAppOrigin('user')}${_.get(params, 'profile.slug')}`;
+  },
+});
+
+routeStore.addRule('activities-history-detail', {
+  url: (params) => {
+    return `/activities-history-detail?id=${_.get(params, 'id')}`;
+  },
+});
+
+// Rule bên dưới add để fix lỗi. Có thể sẽ không có sử dụng!
+
+routeStore.addRule('userProfileMenu', {
+  url: (params) => {
+    return `/me/${params}`;
+  },
+  // parse: (urlObject) => {
+  //   const params = {};
+  //   for (let param in urlObject.searchParams) {
+  //     params[param] = urlObject.searchParams.get(param);
+  //   }
+  //   return params;
+  // },
+  // match: (urlObject) => {
+  //   return urlObject.pathname === 'me/${params}';
+  // },
+});
+routeStore.addRule('coursePurchase', {
+  url: (params) => {
+    let search = `${querystring.stringify(_.pick(params, ['id']))}`;
+    search = search ? `?${search}` : '';
+    return `/course/purchase${search}`;
+  },
+  parse: (urlObject) => {
+    const params = {};
+    for (let param in urlObject.searchParams) {
+      params[param] = urlObject.searchParams.get(param);
+    }
+    return params;
+  },
+  match: (urlObject) => {
+    return urlObject.pathname === 'course/purchase';
   },
 });
