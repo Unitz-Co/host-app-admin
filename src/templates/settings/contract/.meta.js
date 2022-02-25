5c99765210a7556508208a5a4e1e164f6ed2e616
Phuong Vo <vlaphuong1991@gmail.com>
2022-02-25 11:14:08 +0700

    update

---
+++ src/templates/settings/contract/.meta.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const _ = require('lodash');
+require('@vl/mod-config/web');
+
+exports.createPages = async (item, gatsby) => {
+  // @update query
+  return Promise.all(
+    ['index'].map(() => {
+      const pagePath = 'settings/contract';
+      console.log('creating page', pagePath);
+      const pageContext = _.cloneDeep({
+        id: pagePath,
+        slug: pagePath,
+        params: {
+          navbar: {
+            title: 'Settings/Contract',
+            path: 'settings/contract',
+          },
+        },
+      });
+      return gatsby.actions.createPage({
+        path: pagePath,
+        component: item.resolvers.component(gatsby),
+        context: pageContext,
+      });
+    })
+  );
+};
