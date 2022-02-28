ac5da8914b4d64f9a0000abc08a22cb137cfd5fb
Phuong Vo <vlaphuong1991@gmail.com>
2022-02-28 18:04:46 +0700

    wip

---
+++ src/templates/settings/message/.meta.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const _ = require('lodash');
+require('@vl/mod-config/web');
+
+exports.createPages = async (item, gatsby) => {
+  // @update query
+  return Promise.all(
+    ['index'].map(() => {
+      const pagePath = 'settings/message';
+      console.log('creating page', pagePath);
+      const pageContext = _.cloneDeep({
+        id: pagePath,
+        slug: pagePath,
+        params: {
+          navbar: {
+            title: 'Settings/Message',
+            path: 'settings/message',
+          },
+        },
+      });
+      return gatsby.actions.createPage({
+        path: pagePath,
+        component: item.resolvers.component(gatsby),
+        context: pageContext,
+      });
+    })
+  );
+};
---
+++ src/templates/settings/message/.route.js
@@ -0,0 +1,19 @@
+const _ = require('lodash');
+const slugify = require('slugify');
+const { routeStore } = require('@vl/mod-utils/gatsbyRouteStore');
+
+routeStore.addRule('message', {
+  url: (params) => {
+    return `/settings/message`;
+  },
+  parse: (urlObject) => {
+    const params = {};
+    for (let param in urlObject.searchParams) {
+      params[param] = urlObject.searchParams.get(param);
+    }
+    return params;
+  },
+  match: (urlObject) => {
+    return urlObject.pathname === 'message';
+  },
+});
